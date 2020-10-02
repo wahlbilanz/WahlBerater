cd8ca68471285cc9cbd8d2d4024bbc6f4039523d
Martin Peters <martin@freakybytes.net>
2020-10-02 14:54:53 +0200

    chore: scaffolded project structure and added ng-zorro

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import en from '@angular/common/locales/en';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { AppComponent } from './components/app/app.component';
+import { CoreRoutingModule } from './core-routing.module';
+
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent]
+})
+export class CoreModule { }
