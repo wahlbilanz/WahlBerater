b91b5edefe92db225f4d144029d4e0c96314d59f
Martin Peters <martin@freakybytes.net>
2020-11-17 21:06:11 +0100

    fix(#5): added scroll to top effect and reworked main app structure, so the page is scrolled, instead of an element

--- src/app/+state/app.effects.ts
+++ src/app/+state/app.effects.ts
@@ -1,14 +1,15 @@
+import { ViewportScroller } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { State } from './app.models';
+import { NavigationStart } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { routerNavigatedAction, routerRequestAction } from '@ngrx/router-store';
 import { Store } from '@ngrx/store';
-
-import { routerNavigatedAction } from '@ngrx/router-store';
-import { mergeMap, map, catchError } from 'rxjs/operators';
-import * as AppActions from './app.actions';
-import { DataService } from '../core/services/data.service';
 import { asyncScheduler, scheduled } from 'rxjs';
+import { catchError, map, mergeMap, tap } from 'rxjs/operators';
 import { DataPersistanceService } from '../core/services/data-persistance.service';
+import { DataService } from '../core/services/data.service';
+import * as AppActions from './app.actions';
+import { State } from './app.models';
 
 @Injectable()
 export class AppEffects {
@@ -17,6 +18,33 @@ export class AppEffects {
       ofType(routerNavigatedAction),
       map(() => AppActions.toggleMenu({ open: false })),
     ),
+  );
+
+  private readonly scrollLevelStore: { [navigationId: number]: [number, number] } = {};
+  private lastScrollId = 0;
+  private restoredScrollId: number;
+  restoreScrollStateAfterNavigation = createEffect(
+    () =>
+      this.actions.pipe(
+        ofType(routerNavigatedAction, routerRequestAction),
+        tap(({ type, payload }) => {
+          if (type === '@ngrx/router-store/request') {
+            // store scroll position before navigating off
+            this.scrollLevelStore[this.lastScrollId] = this.viewportScroller.getScrollPosition();
+            this.lastScrollId = payload.event.id;
+            // when routing state is restored (cf. RouteReuseStrategy), the scroll state is restored
+            this.restoredScrollId = (payload.event as NavigationStart).restoredState
+              ? (payload.event as NavigationStart).restoredState.navigationId
+              : undefined;
+          } else if (type === '@ngrx/router-store/navigated') {
+            // scroll either to the saved scroll state, or the to top of the page
+            this.viewportScroller.scrollToPosition(
+              this.restoredScrollId != null ? this.scrollLevelStore[this.restoredScrollId] || [0, 0] : [0, 0],
+            );
+          }
+        }),
+      ),
+    { dispatch: false },
   );
 
   loadData = createEffect(
@@ -52,5 +80,6 @@ export class AppEffects {
     private store: Store<State>,
     private dataService: DataService,
     private dataPersistanceService: DataPersistanceService,
+    private viewportScroller: ViewportScroller,
   ) {}
 }
