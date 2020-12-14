9629e4d8847f6b577bddd7a9848dda2cc096e64d
Martin Scharm <martin@binfalse.de>
2020-12-14 23:21:23 +0100

    fixed candidate preview page

--- src/app/modules/candidate/pages/candidate-profile-page/candidate-profile-page.component.ts
+++ src/app/modules/candidate/pages/candidate-profile-page/candidate-profile-page.component.ts
@@ -3,6 +3,9 @@ import { select, Store } from '@ngrx/store';
 import { AppPartialState } from '../../../../+state/app.reducer';
 import * as AppSelectors from '../../../../+state/app.selectors';
 import { ActivatedRoute } from '@angular/router';
+import {CandidatePersonalInfo, PersonalCandidateMap} from '../../../../definitions/models/candidate.model';
+import {PoliticalData} from '../../../../definitions/models/political.data.model';
+import {getCandidatePersonalInfo} from '../../../../definitions/functions/getCandidatePersonalInfo';
 // import {decisionToWord, candidateDecisionToWord} from '../../../../definitions/functions/decision-mapping.function';
 
 @Component({
@@ -11,7 +14,10 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./candidate-profile-page.component.scss'],
 })
 export class CandidateProfilePageComponent implements OnInit {
-  data = this.store.pipe(select(AppSelectors.getPoliticalData));
+  votes = this.store.pipe(select(AppSelectors.getVotes));
+  // personalData: PersonalCandidateMap = undefined;
+  politicalData: PoliticalData = undefined;
+  personalInfo: CandidatePersonalInfo = undefined;
   candidateId: string;
 
   constructor(private store: Store<AppPartialState>, private route: ActivatedRoute) {}
@@ -19,6 +25,13 @@ export class CandidateProfilePageComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe((pm) => {
       this.candidateId = pm.get('candidate_id');
+    });
+    this.store.pipe(select(AppSelectors.getPersonalData)).subscribe(d => {
+      // this.personalData = d;
+      this.personalInfo = getCandidatePersonalInfo(d, this.candidateId);
+    });
+    this.store.pipe(select(AppSelectors.getPoliticalData)).subscribe(d => {
+      this.politicalData = d;
     });
   }
 }
