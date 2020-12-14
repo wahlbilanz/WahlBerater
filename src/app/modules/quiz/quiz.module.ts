1658cec84d5e525c4e7370dbf926fc93cc905151
Martin Scharm <martin@binfalse.de>
2020-12-14 22:08:36 +0100

    starts for favs in panel

--- src/app/modules/quiz/quiz.module.ts
+++ src/app/modules/quiz/quiz.module.ts
@@ -16,6 +16,7 @@ import { PipesModule } from '../pipes/pipes.module';
 import { AuswertungHeatmapComponent } from './auswertung-heatmap/auswertung-heatmap.component';
 import { AuswertungHeatmapVotesComponent } from './auswertung-heatmap-votes/auswertung-heatmap-votes.component';
 import { AuswertungCategoryPanelComponent } from './auswertung-category-panel/auswertung-category-panel.component';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,6 @@ import { AuswertungCategoryPanelComponent } from './auswertung-category-panel/au
     AuswertungHeatmapVotesComponent,
     AuswertungCategoryPanelComponent,
   ],
-  imports: [CommonModule, QuizRoutingModule, NzIconModule, NzCollapseModule, NzListModule, NgApexchartsModule, PipesModule, NzTabsModule, NzToolTipModule],
+  imports: [CommonModule, QuizRoutingModule, NzIconModule, NzCollapseModule, NzListModule, NgApexchartsModule, PipesModule, NzTabsModule, NzToolTipModule,NzBadgeModule],
 })
 export class QuizModule {}
