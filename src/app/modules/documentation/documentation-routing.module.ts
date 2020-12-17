44e98c2a529350d9521f2d1f727c431d857efcd6
Martin Scharm <martin@binfalse.de>
2020-12-18 00:20:03 +0100

    restructured content, created separate imprint

--- src/app/modules/documentation/documentation-routing.module.ts
+++ src/app/modules/documentation/documentation-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component';
+import {ImprintPageComponent} from './pages/imprint-page/imprint-page.component';
 
 const routes: Routes = [
   { path: 'about', component: AboutPageComponent },
   { path: 'privacy', component: PrivacyPageComponent },
+  { path: 'imprint', component: ImprintPageComponent },
 ];
 
 @NgModule({
--- src/app/modules/documentation/documentation.module.ts
+++ src/app/modules/documentation/documentation.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DocumentationRoutingModule } from './documentation-routing.module';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.component';
+import { ImprintPageComponent } from './pages/imprint-page/imprint-page.component';
 
 @NgModule({
-  declarations: [AboutPageComponent, PrivacyPageComponent],
+  declarations: [AboutPageComponent, PrivacyPageComponent, ImprintPageComponent],
   imports: [CommonModule, DocumentationRoutingModule],
 })
 export class DocumentationModule {}
