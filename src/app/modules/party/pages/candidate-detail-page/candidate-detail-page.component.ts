95a2e8488cfa6a42f2edb11ad874e5eb457908a6
Martin Peters <martin@freakybytes.net>
2021-01-05 23:44:56 +0100

    feat: added basic candidate detail view

--- src/app/modules/party/pages/candidate-detail-page/candidate-detail-page.component.ts
+++ src/app/modules/party/pages/candidate-detail-page/candidate-detail-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { map, switchMap } from 'rxjs/operators';
+import { AppPartialState } from 'src/app/+state/app.reducer';
+import * as AppSelectors from '../../../../+state/app.selectors';
 
 @Component({
   selector: 'app-candidate-detail-page',
@@ -6,7 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidate-detail-page.component.scss'],
 })
 export class CandidateDetailPageComponent implements OnInit {
-  constructor() {}
+  public candidateAndPartyId = this.route.params.pipe(
+    map((params) => ({
+      partyId: params.partyId as string,
+      candidateId: params.candidateId as string,
+    })),
+  );
+  public partyData = this.candidateAndPartyId.pipe(
+    switchMap(({ partyId }) => this.state.pipe(select(AppSelectors.getPartyById, { id: partyId }))),
+  );
+  public candidateData = this.candidateAndPartyId.pipe(
+    switchMap(({ candidateId }) => this.state.pipe(select(AppSelectors.getCandidatePersonalDataById, { id: candidateId }))),
+  );
+
+  constructor(private state: Store<AppPartialState>, private route: ActivatedRoute) {}
 
   ngOnInit(): void {}
 }
