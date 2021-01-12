842c9d487fa443f751f9606be6e1256e1dba889b
Martin Peters <martin@freakybytes.net>
2021-01-12 14:09:22 +0100

    fix: fixed nav container not aligning with menu toggle

--- src/app/core/components/app/app.component.ts
+++ src/app/core/components/app/app.component.ts
@@ -1,5 +1,5 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { NzBreakpointService, siderResponsiveMap } from 'ng-zorro-antd/core/services';
 import * as AppActions from '../../../+state/app.actions';
@@ -27,6 +27,9 @@ import * as AppSelectors from '../../../+state/app.selectors';
 })
 export class AppComponent implements OnInit {
   public isMenuOpen = this.store.pipe(select(AppSelectors.isMenuOpen));
+  public navRightOffset = 16;
+
+  @ViewChild('navToggle', { read: ElementRef, static: true }) navToggleElement: ElementRef;
 
   constructor(private store: Store<AppPartialState>, private breakpointService: NzBreakpointService) {
     this.breakpointService.subscribe(siderResponsiveMap, true).subscribe((activeBreakpoints) => {
@@ -34,9 +37,16 @@ export class AppComponent implements OnInit {
     });
   }
 
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.calculateNavOffset();
+  }
+
   ngOnInit() {
     // first things first! Let's load some data.
     this.store.dispatch(AppActions.loadData());
+    // initially calculate offset for nav container
+    this.calculateNavOffset();
   }
 
   toggleMenu(event: MouseEvent) {
@@ -51,5 +61,15 @@ export class AppComponent implements OnInit {
   /** Hides menu, when main container received click */
   mainClicked(event: MouseEvent) {
     this.store.dispatch(AppActions.toggleMenu({ open: false }));
+  }
+
+  private calculateNavOffset() {
+    // check for breakpoint (max-width: $wb-breakpoint-small) must kept in sync with scss!
+    if (document.body.clientWidth <= 576) {
+      this.navRightOffset = 0;
+    } else if (!!this.navToggleElement && !!this.navToggleElement.nativeElement) {
+      const el: HTMLElement = this.navToggleElement.nativeElement;
+      this.navRightOffset = document.body.clientWidth - (el.offsetLeft + el.clientWidth);
+    }
   }
 }
