d5614bde91e7766868da71d3dac2cc4e48a84a59
Martin Peters <martin@freakybytes.net>
2021-01-14 17:26:32 +0100

    feat(#5): added more breadcrumbs

--- src/app/modules/claim/claim.module.ts
+++ src/app/modules/claim/claim.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NzListModule } from 'ng-zorro-antd/list';
+import { NgModule } from '@angular/core';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { CandidateModule } from '../candidate/candidate.module';
+import { PipesModule } from '../pipes/pipes.module';
 import { ClaimRoutingModule } from './claim-routing.module';
 import { ClaimListPageComponent } from './pages/claim-list-page/claim-list-page.component';
 import { ClaimProvPageComponent } from './pages/claim-prov-page/claim-prov-page.component';
-import { CandidateModule } from '../candidate/candidate.module';
-import { PipesModule } from '../pipes/pipes.module';
 
 @NgModule({
   declarations: [ClaimListPageComponent, ClaimProvPageComponent],
-  imports: [CommonModule, ClaimRoutingModule, NzListModule, CandidateModule, NzIconModule, PipesModule],
+  imports: [CommonModule, ClaimRoutingModule, NzListModule, CandidateModule, NzIconModule, PipesModule, NzBreadCrumbModule],
 })
 export class ClaimModule {}
