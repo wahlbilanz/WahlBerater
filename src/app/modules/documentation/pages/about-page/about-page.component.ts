37820f7b0a6fe3a793fe145cf4734b41e8e0b0c2
Martin Scharm <martin@binfalse.de>
2021-03-21 00:18:35 +0100

    started static pages

--- src/app/modules/documentation/pages/about-page/about-page.component.ts
+++ src/app/modules/documentation/pages/about-page/about-page.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface FaqEntry {
+  question: string;
+  answer: string;
+}
+
 @Component({
   selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss'],
 })
 export class AboutPageComponent implements OnInit {
-  constructor() {}
+  faq: FaqEntry[] = [];
+
+  constructor() {
+    this.faq.push({
+      question: 'Was soll ich fragen?',
+      answer: 'Vielleicht fragst du das am besten jemand anderes...',
+    });
+  }
 
   ngOnInit(): void {}
 }
