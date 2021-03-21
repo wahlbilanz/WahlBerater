f01dfa229ee8e466ae230549a354b9c45e1587f7
Martin Scharm <martin@binfalse.de>
2021-03-21 01:24:53 +0100

    updated datenschutzerklaerung

--- src/app/modules/documentation/pages/privacy-page/privacy-page.component.ts
+++ src/app/modules/documentation/pages/privacy-page/privacy-page.component.ts
@@ -1,12 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import * as AppSelectors from '../../../../+state/app.selectors';
+import { Subscription } from 'rxjs';
+import { AccessibilityModes, AccessibleUrlFragment } from 'src/app/+state/app.models';
+import { AppPartialState } from 'src/app/+state/app.reducer';
 
 @Component({
   selector: 'app-privacy-page',
   templateUrl: './privacy-page.component.html',
   styleUrls: ['./privacy-page.component.scss'],
 })
-export class PrivacyPageComponent implements OnInit {
-  constructor() {}
+export class PrivacyPageComponent implements OnInit, AfterViewInit, OnDestroy {
+  private fragment?: string;
+  public accessibilityModes?: AccessibilityModes;
+  public sAccessibleUrlFragment = AccessibleUrlFragment;
+  private subscriptions: Subscription[] = [];
+  constructor(private store: Store<AppPartialState>, private route: ActivatedRoute) {
+    this.subscriptions.push(
+      this.store.pipe(select(AppSelectors.getAllAccessibilityModes)).subscribe((am) => (this.accessibilityModes = am)),
+    );
+  }
 
-  ngOnInit(): void {}
+  ngOnDestroy() {
+    for (const s of this.subscriptions) {
+      s.unsubscribe();
+    }
+  }
+
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.route.fragment.subscribe((fragment) => {
+        this.fragment = fragment;
+      }),
+    );
+  }
+
+  ngAfterViewInit(): void {
+    if (this.fragment) {
+      try {
+        if (this.accessibilityModes?.reducedMotionMode) {
+          document.querySelector('#' + this.fragment)?.scrollIntoView();
+        } else {
+          setTimeout(() => {
+            document.querySelector('#' + this.fragment)?.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start',
+              inline: 'nearest',
+            });
+          }, 100);
+        }
+      } catch (e) {}
+    }
+  }
 }
