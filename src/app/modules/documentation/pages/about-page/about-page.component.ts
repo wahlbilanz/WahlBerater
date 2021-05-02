2673a62567f04c3787cae8c0c82045113290c8f4
Martin Scharm <martin@binfalse.de>
2021-05-02 23:34:40 +0200

    bit more faq

--- src/app/modules/documentation/pages/about-page/about-page.component.ts
+++ src/app/modules/documentation/pages/about-page/about-page.component.ts
@@ -28,51 +28,21 @@ export class AboutPageComponent implements OnInit {
         answer: '',
       },
       {
-        question: 'Welcher Liste gehoert ihr an?',
-        answer: 'Wir sind parteilos und haben einfach nur spass an Politik. Wir moechten die Demokratie staerken.',
+        question: 'Welcher Liste gehört ihr an?',
+        answer:
+          'Wir gehören keiner Liste an und kennen die Kandidierenden auch nicht. ' +
+          'Wir sind parteilos und haben einfach nur Spaß an Politik und Demokratie.',
       },
       {
         question: 'Wo habt ihr die Thesen her?',
         answer: '',
       },
       {
-        question: 'Wie habt ihr die Thesen ausgewaehlt?',
+        question: 'Wie habt ihr die Thesen ausgewählt?',
         answer: '',
       },
       {
-        question: 'wie kommt die reihenfolge der listen/candidaten etc in den verschiedenen ansichten zustande?',
-        answer: '',
-      },
-      {
-        question: 'welche Daten sammelt ihr?',
-        answer: '',
-      },
-      {
-        question: 'wer sieht wie ich mich zu einer These entschieden habe?',
-        answer: '',
-      },
-      {
-        question: 'mit wem teilt ihr meine Daten?',
-        answer: '',
-      },
-      {
-        question: 'kann ich den Code zur App bekommen?',
-        answer: '',
-      },
-      {
-        question: 'wie kann ich meine Daten löschen?',
-        answer: '',
-      },
-      {
-        question: 'Wo kann ich mich registrieren?',
-        answer: '',
-      },
-      {
-        question: 'Kann ich auch einen WahlBerater bekommen?',
-        answer: '',
-      },
-      {
-        question: 'wie berechnet ihr die auswertung?',
+        question: 'Wie kommt die Reihenfolge der Listen/Kandidierenden in den verschiedenen Ansichten zustande?',
         answer: '',
       },
       {
@@ -85,6 +55,45 @@ export class AboutPageComponent implements OnInit {
           'Seitdem entwickeln wir das Tool als Dienstleister für den AStA.' +
           'Unsere Berührungspunkte mit dem AStA beschränken sich jedoch lediglich auf organisatorische Angelegenheiten &mdash; inhaltlich waren wir vollkommen unabhängig.' +
           'Der AStA war nicht in die Thesengenerierung und -auswahl einbezogen und hat die Thesen auch erst am Tag der Veröffentlichung gesehen.',
+      },
+      {
+        question: 'wie berechnet ihr die auswertung?',
+        answer: '',
+      },
+      {
+        question: 'Welche Daten sammelt ihr?',
+        answer: '',
+      },
+      {
+        question: 'Wer sieht wie ich mich zu einer These entschieden habe?',
+        answer: '',
+      },
+      {
+        question: 'mit wem teilt ihr meine Daten?',
+        answer: '',
+      },
+      {
+        question: 'wie kann ich meine Daten löschen?',
+        answer: '',
+      },
+      {
+        question: 'kann ich den Code zur App bekommen?',
+        answer: '',
+      },
+      {
+        question: 'Wo kann ich mich registrieren?',
+        answer:
+          'Ähm, wofür willst du dich nochmal registrieren? ' +
+          'Hier gibt es keine Accounts und zum Kandidieren kommst du ein bisschen zu spät.',
+      },
+      {
+        question: 'Kann ich auch einen WahlBerater bekommen?',
+        answer:
+          'Klar! Der WahlBerater ist Quelloffen und steht unter einer *freien* Lizenz zur Verfügung! Du kannst ihn wirklich benutzen wie und wofür du möchtest!  ' +
+          '\n\n' +
+          'Die Entwicklung einer Wahlentscheidungshilfe ist jedoch wesentlich aufwändiger als es im ersten Moment scheint! ' +
+          'Wir helfen dir aber gern als unabhängiges Team. ' +
+          'Schreib uns einfach eine E-Mail wofür du den WahlBerater brauchst und wir finden dann sicher auch eine Lösung für dich :)',
       },
     ];
   }
