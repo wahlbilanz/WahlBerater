08e9c6dec63be356f880bb3dba978efe26fc3ee2
Martin Scharm <martin@binfalse.de>
2021-05-02 23:34:40 +0200

    started faq

--- src/app/modules/documentation/documentation.module.ts
+++ src/app/modules/documentation/documentation.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzCollapseModule } from 'ng-zorro-antd/collapse';
+import { HelpersModule } from '../helpers/helpers.module';
+import { PipesModule } from '../pipes/pipes.module';
 import { DocumentationRoutingModule } from './documentation-routing.module';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { ImprintPageComponent } from './pages/imprint-page/imprint-page.component';
@@ -9,6 +11,6 @@ import { PrivacyPageComponent } from './pages/privacy-page/privacy-page.componen
 
 @NgModule({
   declarations: [AboutPageComponent, PrivacyPageComponent, ImprintPageComponent],
-  imports: [CommonModule, DocumentationRoutingModule, NzBreadCrumbModule, NzCollapseModule],
+  imports: [CommonModule, DocumentationRoutingModule, NzBreadCrumbModule, NzCollapseModule, PipesModule],
 })
 export class DocumentationModule {}
--- src/app/modules/documentation/pages/about-page/about-page.component.ts
+++ src/app/modules/documentation/pages/about-page/about-page.component.ts
@@ -14,10 +14,79 @@ export class AboutPageComponent implements OnInit {
   faq: FaqEntry[] = [];
 
   constructor() {
-    this.faq.push({
-      question: 'Was soll ich fragen?',
-      answer: 'Vielleicht fragst du das am besten jemand anderes...',
-    });
+    this.faq = [
+      {
+        question: 'Was soll ich fragen?',
+        answer: 'Vielleicht fragst du das am besten jemand anderes...',
+      },
+      {
+        question: 'Wie kommt ihr auf den Namen?',
+        answer: 'Wir haben locker 100+ Vorschläge diskutiert. Am Ende gab es natürlich eine demokratische Entscheidung! :)',
+      },
+      {
+        question: 'Wer seid ihr?',
+        answer: '',
+      },
+      {
+        question: 'Welcher Liste gehoert ihr an?',
+        answer: 'Wir sind parteilos und haben einfach nur spass an Politik. Wir moechten die Demokratie staerken.',
+      },
+      {
+        question: 'Wo habt ihr die Thesen her?',
+        answer: '',
+      },
+      {
+        question: 'Wie habt ihr die Thesen ausgewaehlt?',
+        answer: '',
+      },
+      {
+        question: 'wie kommt die reihenfolge der listen/candidaten etc in den verschiedenen ansichten zustande?',
+        answer: '',
+      },
+      {
+        question: 'welche Daten sammelt ihr?',
+        answer: '',
+      },
+      {
+        question: 'wer sieht wie ich mich zu einer These entschieden habe?',
+        answer: '',
+      },
+      {
+        question: 'mit wem teilt ihr meine Daten?',
+        answer: '',
+      },
+      {
+        question: 'kann ich den Code zur App bekommen?',
+        answer: '',
+      },
+      {
+        question: 'wie kann ich meine Daten löschen?',
+        answer: '',
+      },
+      {
+        question: 'Wo kann ich mich registrieren?',
+        answer: '',
+      },
+      {
+        question: 'Kann ich auch einen WahlBerater bekommen?',
+        answer: '',
+      },
+      {
+        question: 'wie berechnet ihr die auswertung?',
+        answer: '',
+      },
+      {
+        question: 'Wer finanziert euch?',
+        answer:
+          'Nachdem der AStA der FernUniverstität sich für eine Wahlentscheidungshilfe entschied, hat er die Entwicklung einer solchen ausgeschrieben. ' +
+          'Als Entwickler von [DeinWal.de](https://deinwal.de) wurden wir im Mai 2020 darüber in Kenntnis gesetzt, ' +
+          'waren sofort interessiert, haben ein Konzept und Angebot eingereicht und konnten uns offensichtlich bei der Begutachtung durchsetzen.  ' +
+          '\n\n\n\n' +
+          'Seitdem entwickeln wir das Tool als Dienstleister für den AStA.' +
+          'Unsere Berührungspunkte mit dem AStA beschränken sich jedoch lediglich auf organisatorische Angelegenheiten &mdash; inhaltlich waren wir vollkommen unabhängig.' +
+          'Der AStA war nicht in die Thesengenerierung und -auswahl einbezogen und hat die Thesen auch erst am Tag der Veröffentlichung gesehen.',
+      },
+    ];
   }
 
   ngOnInit(): void {}
