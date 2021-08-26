bafcefa0bd24178436537a28011e2f59c84d144a
Martin Scharm <martin@binfalse.de>
2021-08-26 22:13:34 +0200

    improved auswertungschart

--- src/app/modules/quiz/auswertung-barchart-table/auswertung-barchart-table.component.ts
+++ src/app/modules/quiz/auswertung-barchart-table/auswertung-barchart-table.component.ts
@@ -38,7 +38,7 @@ export class AuswertungBarchartTableComponent implements OnInit {
     }
   }
 
-  sortedCategroies: Observable<CategoryWithClaims[]> = this.store.pipe(select(AppSelectors.getCategoriesWithClaims));
+  // sortedCategroies: Observable<CategoryWithClaims[]> = this.store.pipe(select(AppSelectors.getCategoriesWithClaims));
 
   showCandidateBars = false;
   showCandidatesNum = 0;
@@ -56,7 +56,6 @@ export class AuswertungBarchartTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.recalcAxes();
-    // this.maxValueArray = [...Array(Math.ceil(Math.max(this.partyScoreResult.maxValue, this.partyScoreResult.maxParty))).keys()];
     this.displayCandidates = this.partyScoreResult.partyScores.map((_) => 7);
     this.nCandidates = this.partyScoreResult.partyScores.map((s) => Object.keys(s.candidates).length);
   }
@@ -84,5 +83,48 @@ export class AuswertungBarchartTableComponent implements OnInit {
       }
       this.ref.markForCheck();
     }, RenderingDelay);
+  }
+
+  public getBackground(partyColor: string, index: number): string {
+    if (index % 2) {
+      return partyColor;
+    } else {
+      const otherColor =
+        partyColor === '#000000' || partyColor === '#000' ? '#555' : AuswertungBarchartTableComponent.shadeColor(partyColor, -20);
+      const direction = '-45deg';
+      return (
+        'repeating-linear-gradient(' +
+        direction +
+        ',' +
+        partyColor +
+        ',' +
+        partyColor +
+        ' 5px,' +
+        otherColor +
+        ' 5px,' +
+        otherColor +
+        ' 10px)'
+      );
+    }
+  }
+
+  static shadeColor(color: string, percent: number) {
+    let R = parseInt(color.substring(1, 3), 16);
+    let G = parseInt(color.substring(3, 5), 16);
+    let B = parseInt(color.substring(5, 7), 16);
+
+    R = Math.round((R * (100 + percent)) / 100);
+    G = Math.round((G * (100 + percent)) / 100);
+    B = Math.round((B * (100 + percent)) / 100);
+
+    R = R < 255 ? R : 255;
+    G = G < 255 ? G : 255;
+    B = B < 255 ? B : 255;
+
+    const RR = R.toString(16).length === 1 ? '0' + R.toString(16) : R.toString(16);
+    const GG = G.toString(16).length === 1 ? '0' + G.toString(16) : G.toString(16);
+    const BB = B.toString(16).length === 1 ? '0' + B.toString(16) : B.toString(16);
+
+    return '#' + RR + GG + BB;
   }
 }
