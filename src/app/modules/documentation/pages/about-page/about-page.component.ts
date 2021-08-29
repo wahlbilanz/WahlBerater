9af7fa92aeac1350194c2cb43f0ef294f5fa17dc
Martin Scharm <martin@binfalse.de>
2021-08-29 23:19:38 +0200

    randomized people in about

--- src/app/modules/documentation/pages/about-page/about-page.component.ts
+++ src/app/modules/documentation/pages/about-page/about-page.component.ts
@@ -11,7 +11,15 @@ export class AboutPageComponent implements OnInit {
   domain = 'deinwal';
   mto = 'mailto:';
 
-  constructor() {}
+  contributers: string[] = [];
+
+  constructor() {
+    this.contributers = [
+      '<a href="https://www.linkedin.com/in/sophie-flack-36826121b">Sophie Flack</a> studiert im Master Demokratiewissenschaften an der Universität in Regensburg.',
+      'Tom Theile arbeitet am <a href="https://www.demogr.mpg.de/">Max-Planck-Institut für demographische Forschung</a> als Softwareentwickler.',
+      '<a href="https://binfalse.de">Martin Scharm</a> ist <a href="https://codeahoi.de/">freiberuflicher IT-Ingenieur</a> und Teil eines <a href="https://naval-architect.de/">Schiffbau-Start-Ups.</a>',
+    ].sort(() => 0.5 - Math.random());
+  }
 
   ngOnInit(): void {}
 }
